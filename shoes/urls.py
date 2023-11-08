93de8cce41736d34460e4dc0fba2e21c47f7396d
mahsaaarabi <mahsaaarabi950@gmail.com>
2023-11-08 21:48:27 +0330

    Version 1.2

---
+++ shoes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from shoes import views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name="home"),
+    path('index/', views.HomeView.as_view(), name="home"),
+    path('cart/', views.CartView.as_view(), name="cart"),
+]
---
+++ static/js/product.js
@@ -0,0 +1,10 @@
+var ProductBtn = document.querySelectorAll("#add_product_btn");
+
+ProductBtn.forEach((r) => {
+    r.addEventListener("click", function (e) {
+        let ProductImage = document.getElementById(`image${e.target.value}`);
+        let ProductName = document.getElementById(`name${e.target.value}`);
+        let ProductPrice = document.getElementById(`price${e.target.value}`);
+        localStorage.setItem(`${e.target.value}`, `${ProductImage.src.substring(22, 90)},${ProductName.innerText},${ProductPrice.innerText}`);
+    });
+});
